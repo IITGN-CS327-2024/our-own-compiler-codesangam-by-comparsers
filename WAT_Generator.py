6f6f2e0740d7d6c14e11a3049659f79672b4ab5d
dg_20104 <amitgupta@iign.ac.in>
2024-04-22 02:25:00 +0530

    WAT initial

--- WAT_Generator.py
+++ WAT_Generator.py
@@ -2,19 +2,24 @@ from ast_classes import *
 from symbol_table import *
 from error_handling import *
 
-last_scope_index = -1
+func_num = 0
+
+# def write_local_vars(node, file, indent):
 
 def convert_function(line_node, scope_tree, file, indent):
-    file.write("\t"*indent + '(func (export "{}")\n'.format(line_node.children2))
+    file.write("\t"*indent + '(func ${} (export "{}")")\n'.format(line_node.children2))
     indent+=1
     func_type = scope_tree.root.table[line_node.children2]
     num_inputs = len(func_type.inputs)
     for i in range(num_inputs):
         file.write("\t"*indent + "(params ${} i32)\n".format(func_type.input_names[i]))
-    if scope_tree.root.has_return:
+    if func_type.outputs!='void':
         file.write("\t"*indent + "(return i32)\n")
-    last_scope_index = last_scope_index + 1
-    function_node = scope_tree.root.children[last_scope_index]
+    func_node = scope_tree.root.children[func_num]
+    # write_local_vars(func_node, file, indent)
+    for i in range(4, line_node.num_child):
+        in_line = getattr(line_node, "children{}".format(i))
+        convert_line(line_node, scope_tree, file, indent)
     file.write("\t"*(indent-1) + ")\n")
 
 def convert_line(line_node, scope_tree, file, indent):
@@ -22,7 +27,7 @@ def convert_line(line_node, scope_tree, file, indent):
         convert_assignment(line_node, scope_tree, indent)
     elif isinstance(line_node, function):
         convert_function(line_node, scope_tree, file, indent)
-    return
+        func_num+=1
 
 def convert_program(node, scope_tree, file_name):
     with open('WAT_Code/{}.wat'.format(file_name), 'w') as file:
